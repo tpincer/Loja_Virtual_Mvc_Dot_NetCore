a3bbb7c120f5a34d6ee741fd16d6996c86240864
tpincer <47256731+tpincer@users.noreply.github.com>
2020-06-22 19:33:44 -0300

    Fim do formulário de contato
    Fim do formulário de contato

--- Seção 08/01_Fim/LojaVirtual/Controllers/HomeController.cs
+++ Seção 08/01_Fim/LojaVirtual/Controllers/HomeController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace LojaVirtual.Controllers {
     public class HomeController : Controller {
@@ -14,6 +18,55 @@ namespace LojaVirtual.Controllers {
         public IActionResult Contato()
         {
             return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var listaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagens, true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var texto in listaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                ViewData["MSG_E"] = "Ops! Tivemos um problema, tente novamente mais tarde!";
+
+                //TODO - implementar log
+            }
+
+
+
+            //return new ContentResult() { Content = string.Format("Dados recebidos com sucesso!<br/> Nome: {0} <br/>E-mail: {1} <br/>Texto: {2}", nome, email, texto), ContentType = "text/html" };
+
+
+
+            return View("Contato");
         }
 
         public IActionResult Login()
