20e4812faeadcc4cf639acf260a0d5bc84847b77
tpincer <47256731+tpincer@users.noreply.github.com>
2020-09-26 10:48:36 -0300

    Validação de login
    Validação de login

---
+++ Seção 12/02_Fim/LojaVirtual/Repositories/Contracts/IColaboradorRepository.cs
@@ -0,0 +1,20 @@
+﻿using LojaVirtual.Migrations;
+using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Repositories.Contracts
+{
+    interface IColaboradorRepository
+    {
+        Colaborador Login(string email, string senha);
+        void Cadastrar(Colaborador colaborador);
+        void Atualizar(Colaborador colaborador);
+        void Excluir(int Id);
+
+        Colaborador ObterColaborador(int Id);
+        IEnumerable<Colaborador> ObterColaboradores();
+    }
+}
